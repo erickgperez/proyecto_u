a3f61d49c31aa27bab1f5574399f3e1480ace423
Erick Pérez <erick.gperez@outlook.com>
2025-09-11 14:51:44 -0600

    Refactorizar formulario de ingreso

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -2,7 +2,6 @@
 import InputError from '@/components/InputError.vue';
 import TextLink from '@/components/TextLink.vue';
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthBase from '@/layouts/AuthLayout.vue';
@@ -28,8 +27,8 @@ const submit = () => {
 </script>
 
 <template>
-    <AuthBase title="Log in to your account" description="Enter your email and password below to log in">
-        <Head title="Log in" />
+    <AuthBase :title="$t('_inicia_session_en_tu_cuenta_')" :description="$t('_ingrese_email_clave_para_conectarse_')">
+        <Head :title="$t('_ingresar_')" />
 
         <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
             {{ status }}
@@ -38,7 +37,7 @@ const submit = () => {
         <form @submit.prevent="submit" class="flex flex-col gap-6">
             <div class="grid gap-6">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
+                    <Label for="email">{{ $t('_direccion_email_') }}</Label>
                     <Input
                         id="email"
                         type="email"
@@ -54,9 +53,9 @@ const submit = () => {
 
                 <div class="grid gap-2">
                     <div class="flex items-center justify-between">
-                        <Label for="password">Password</Label>
+                        <Label for="password">{{ $t('_clave_') }}</Label>
                         <TextLink v-if="canResetPassword" :href="route('password.request')" class="text-sm" :tabindex="5">
-                            Forgot password?
+                            {{ $t('_olvido_clave_') }}
                         </TextLink>
                     </div>
                     <Input
@@ -66,27 +65,27 @@ const submit = () => {
                         :tabindex="2"
                         autocomplete="current-password"
                         v-model="form.password"
-                        placeholder="Password"
+                        :placeholder="$t('_clave_')"
                     />
                     <InputError :message="form.errors.password" />
                 </div>
 
-                <div class="flex items-center justify-between">
+                <!-- <div class="flex items-center justify-between">
                     <Label for="remember" class="flex items-center space-x-3">
                         <Checkbox id="remember" v-model="form.remember" :tabindex="3" />
                         <span>Remember me</span>
                     </Label>
-                </div>
+                </div>-->
 
                 <Button type="submit" class="mt-4 w-full" :tabindex="4" :disabled="form.processing">
                     <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
-                    Log in
+                    {{ $t('_ingresar_') }}
                 </Button>
             </div>
 
             <div class="text-center text-sm text-muted-foreground">
-                Don't have an account?
-                <TextLink :href="route('register')" :tabindex="5">Sign up</TextLink>
+                {{ $t('_no_tiene_una_cuenta_') }}
+                <TextLink :href="route('register')" :tabindex="5">{{ $t('_registrarse_') }}</TextLink>
             </div>
         </form>
     </AuthBase>
