89b7bc0f1a307b2a116ece8d51ffe55552a53685
Erick Pérez <erick.gperez@outlook.com>
2025-09-19 16:52:01 -0600

    Envio de enlace para restablecer contraseña

---
+++ lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Su clave ha sido restablecida.',
+    'sent' => 'Se ha enviado su enlace para restablecer clave.',
+    'throttled' => 'Por favor, espere antes de volver a intentar.',
+    'token' => 'El token para restablecer clave no es válido.',
+    'user' => "No se ha encontrado un usuario con esa dirección de correo.",
+
+];
--- resources/js/pages/auth/ForgotPassword.vue
+++ resources/js/pages/auth/ForgotPassword.vue
@@ -22,8 +22,8 @@ const submit = () => {
 </script>
 
 <template>
-    <AuthLayout title="Forgot password" description="Enter your email to receive a password reset link">
-        <Head title="Forgot password" />
+    <AuthLayout :title="$t('auth._olvido_clave_')" :description="$t('auth._ingrese_email_para_restablecer_')">
+        <Head :title="$t('auth._olvido_clave_')" />
 
         <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
             {{ status }}
@@ -32,7 +32,7 @@ const submit = () => {
         <div class="space-y-6">
             <form @submit.prevent="submit">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
+                    <Label for="email">{{ $t('auth._direccion_email_') }}</Label>
                     <Input id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus placeholder="email@example.com" />
                     <InputError :message="form.errors.email" />
                 </div>
@@ -40,14 +40,14 @@ const submit = () => {
                 <div class="my-6 flex items-center justify-start">
                     <Button class="w-full" :disabled="form.processing">
                         <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
-                        Email password reset link
+                        {{ $t('auth._enviar_enlace_') }}
                     </Button>
                 </div>
             </form>
 
             <div class="space-x-1 text-center text-sm text-muted-foreground">
-                <span>Or, return to</span>
-                <TextLink :href="route('login')">log in</TextLink>
+                <span>{{ $t('auth._o_retornar_a_') }}</span>
+                <TextLink :href="route('login')">{{ $t('auth._ingresar_') }}</TextLink>
             </div>
         </div>
     </AuthLayout>
