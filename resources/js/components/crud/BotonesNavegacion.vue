87b2a8b93701aa6bd4b9578a88bea8e804929824
Erick Pérez <erick.gperez@outlook.com>
2025-09-18 23:39:46 -0600

    Crear archivo para botones de navegación del crud

---
+++ resources/js/components/crud/BotonesNavegacion.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const emit = defineEmits(['nextStep']);
+
+const props = defineProps({
+    selectedAction: {
+        type: String,
+        required: true,
+        default: () => '',
+    },
+    step: {
+        type: Number,
+        required: true,
+    },
+});
+</script>
+<template>
+    <v-btn
+        v-if="props.step > 1 && props.selectedAction != 'new'"
+        prepend-icon="mdi-arrow-left-bold"
+        rounded
+        variant="tonal"
+        color="blue-darken-4"
+        @click="emit('nextStep', props.step - 1)"
+    >
+        <template v-slot:prepend>
+            <v-icon color="success"></v-icon>
+        </template>
+        {{ $t('_atras_') }}
+    </v-btn>
+    <v-btn v-if="props.step == 3" prepend-icon="mdi-page-first" rounded variant="tonal" color="blue-darken-4" @click="emit('nextStep', 1)">
+        <template v-slot:prepend>
+            <v-icon color="success"></v-icon>
+        </template>
+        {{ $t('_regresar_listado_') }}
+    </v-btn>
+</template>
