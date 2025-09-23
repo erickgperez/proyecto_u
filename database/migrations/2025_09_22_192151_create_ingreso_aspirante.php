e0dda4bdac2d67cbf4730467e9feca6743106c9b
Erick Pérez <erick.gperez@outlook.com>
2025-09-23 13:29:07 -0600

    Creación de tablas de aspirante y su relación con convocatoria

---
+++ database/migrations/2025_09_22_192151_create_ingreso_aspirante.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ingreso_aspirante', function (Blueprint $table) {
+            $table->id();
+
+            $table->comment('Tabla de aspirantes a una convocatoria de ingreso');
+            $table->foreignId('persona_id');
+            $table->foreign('persona_id')->references('id')->on('public.persona')->onDelete('CASCADE')->onUpdate('CASCADE');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingreso_aspirante');
+    }
+};
---
+++ database/migrations/2025_09_23_191958_create_ingreso_convocatoria_aspirante.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ingreso.convocatoria_aspirante', function (Blueprint $table) {
+            $table->id();
+
+            $table->comment('Relación de muchos a muchos entre convocatoria y aspirante');
+
+            $table->foreignId('convocatoria_id');
+            $table->foreign('convocatoria_id')->references('id')->on('ingreso.convocatoria')->onDelete('CASCADE')->onUpdate('CASCADE');
+
+            $table->foreignId('aspirante_id');
+            $table->foreign('aspirante_id')->references('id')->on('ingreso.aspirante')->onDelete('CASCADE')->onUpdate('CASCADE');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingreso.convocatoria_aspirante');
+    }
+};
