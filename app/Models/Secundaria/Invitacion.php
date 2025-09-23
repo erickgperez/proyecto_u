e134e65ab27e950a25c3eb12fcea5a5d5dc8c2d1
Erick Pérez <erick.gperez@outlook.com>
2025-09-23 12:59:03 -0600

    Agregar campos de auditoria al modelo

--- app/Models/Secundaria/Invitacion.php
+++ app/Models/Secundaria/Invitacion.php
@@ -2,15 +2,36 @@
 
 namespace App\Models\Secundaria;
 
+use App\Models\Ingreso\Convocatoria;
+use App\Models\User;
+use App\Traits\UserStamps;
 use Illuminate\Database\Eloquent\Model;
 
 class Invitacion extends Model
 {
+    use UserStamps;
     //
     protected $table = "secundaria.invitacion";
 
     protected $fillable = [
         'nie',
-        'codigo'
+        'codigo',
+        'convocatoria_id',
+        'invitado'
     ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function convocatoria()
+    {
+        return $this->belongsTo(Convocatoria::class);
+    }
 }
