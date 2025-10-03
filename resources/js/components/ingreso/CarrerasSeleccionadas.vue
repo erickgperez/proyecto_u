13dce5c7a481449a300f509eeb2e9396ea72b1d4
Erick Pérez <erick.gperez@outlook.com>
2025-10-03 10:37:02 -0600

    Pantalla de resumen para confirmación de datos

---
+++ resources/js/components/ingreso/CarrerasSeleccionadas.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps(['carrerasSeleccionadas']);
+</script>
+<template>
+    <div v-if="props.carrerasSeleccionadas.length > 0">
+        <v-icon color="success" size="x-large" icon="mdi-numeric-1-box"></v-icon> {{ $t('ingreso._primera_opcion_') }}:
+        <span class="text-success text-high-emphasis font-weight-black">{{ props.carrerasSeleccionadas[0].title }}</span>
+    </div>
+    <div v-if="props.carrerasSeleccionadas.length > 1">
+        <v-icon color="warning" size="x-large" icon="mdi-numeric-2-box"></v-icon> {{ $t('ingreso._segunda_opcion_') }}:
+        <span class="text-warning text-high-emphasis font-weight-black">{{ props.carrerasSeleccionadas[1].title }}</span>
+    </div>
+    <div v-if="props.carrerasSeleccionadas.length > 2">
+        <v-icon color="primary" size="x-large" icon="mdi-numeric-3-box"></v-icon> {{ $t('ingreso._tercera_opcion_') }}:
+        <span class="text-high-emphasis font-weight-black text-primary"> {{ props.carrerasSeleccionadas[2].title }}</span>
+    </div>
+</template>
--- resources/js/components/ingreso/SeleccionProcesoForm.vue
+++ resources/js/components/ingreso/SeleccionProcesoForm.vue
@@ -3,16 +3,25 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 import { onMounted, ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
-import type { VForm } from 'vuetify/components';
+import CarrerasSeleccionadas from './CarrerasSeleccionadas.vue';
 
 const { t } = useI18n();
 
 const loading = ref(false);
 const step = ref(1);
-const formRef = ref<VForm | null>(null);
 const convocatorias = ref([]);
 const carreras = ref([]);
 const sedes = ref([]);
+const oferta = ref([]);
+const ofertaSede = ref([]);
+const snackbar = ref(false);
+const text = ref('');
+const form1 = ref(null);
+const form2 = ref(null);
+const form3 = ref(null);
+const convocatoria = ref(null);
+const sede = ref(null);
+const carrerasSeleccionadas = ref([]);
 
 interface FormData {
     convocatoria_id: number | null;
@@ -29,74 +38,87 @@ const formData = ref<FormData>({
 });
 
 async function submitForm() {
-    const { valid } = await formRef.value!.validate();
     loading.value = true;
 
     const hasError = ref(false);
     const message = ref('');
 
-    if (valid) {
-        //formData.value.cuerpo_mensaje = content.value;
-        try {
-            const resp = await axios.postForm(route('ingreso-convocatoria-save'), formData.value, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            if (resp.data.status == 'ok') {
-                Swal.fire({
-                    title: t('_exito_'),
-                    text: t('_datos_subidos_correctamente_'),
-                    icon: 'success',
-                    position: 'top-end',
-                    showConfirmButton: false,
-                    timer: 2500,
-                    toast: true,
-                });
-            } else {
-                hasError.value = true;
-                message.value = t(resp.data.message);
-            }
-        } catch (error: any) {
-            hasError.value = true;
-            message.value = t('_no_se_pudo_guardar_formulario_');
-            console.log(error);
-        }
-
-        if (hasError.value) {
+    console.log(carrerasSeleccionadas.value);
+    formData.value.carrera_sede = carrerasSeleccionadas.value.map((carr) => carr.id);
+    console.log(formData.value);
+    /*try {
+        const resp = await axios.post(route('ingreso-convocatoria-save'), formData.value);
+        if (resp.data.status == 'ok') {
             Swal.fire({
-                title: t('_error_'),
-                text: message.value,
-                icon: 'error',
-                confirmButtonColor: '#D7E1EE',
+                title: t('_exito_'),
+                text: t('_datos_subidos_correctamente_'),
+                icon: 'success',
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 2500,
+                toast: true,
             });
+        } else {
+            hasError.value = true;
+            message.value = t(resp.data.message);
         }
+    } catch (error: any) {
+        hasError.value = true;
+        message.value = t('_no_se_pudo_guardar_formulario_');
+        console.log(error);
     }
+
+    if (hasError.value) {
+        Swal.fire({
+            title: t('_error_'),
+            text: message.value,
+            icon: 'error',
+            confirmButtonColor: '#D7E1EE',
+        });
+    }*/
+
     loading.value = false;
+}
+
+async function validarCarreras() {
+    if (carrerasSeleccionadas.value.length > 0) {
+        const continuar = ref(true);
+
+        if (carrerasSeleccionadas.value.length < 3) {
+            continuar.value = false;
+            Swal.fire({
+                title: t('ingreso._carreras_incompletas_'),
+                text: t('ingreso._no_eleccion_carreras_alternativas_'),
+                showCancelButton: true,
+                confirmButtonText: t('_continuar_'),
+                cancelButtonText: t('_cancelar_'),
+                confirmButtonColor: '#e5adac',
+                cancelButtonColor: '#D7E1EE',
+            }).then(async (result) => {
+                if (result.isConfirmed) {
+                    step.value++;
+                }
+            });
+        } else {
+            step.value++;
+        }
+    } else {
+        text.value = t('ingreso._debe_seleccionar_al_menos_una_carrera_');
+        snackbar.value = true;
+    }
 }
 
 onMounted(() => {
     axios
         .get(route('ingreso-aspirante-convocatoria-carrera', { id: props.aspirante.id }))
         .then(function (response) {
-            //console.log(response.data);
-
             convocatorias.value = response.data.convocatorias;
             carreras.value = response.data.carreras;
-            //sedes.value = response.data.sedes;
         })
         .catch(function (error) {
-            // handle error
             console.error('Error fetching data:', error);
         });
 });
-
-const oferta = ref([]);
-const ofertaSede = ref([]);
-const carrerasSeleccionadas = ref([]);
-const form1 = ref(null);
-const form2 = ref(null);
-const form3 = ref(null);
 
 const forms = [form1, form2, form3];
 
@@ -107,15 +129,13 @@ async function nextStep() {
         if (valid) {
             step.value++;
         }
-        if (step.value === 2 && formData.value.convocatoria_id != null) {
-            const resp = await axios.get(route('ingreso-convocatoria-oferta', { id: formData.value.convocatoria_id }));
+        if (step.value === 2 && convocatoria.value != null) {
+            const resp = await axios.get(route('ingreso-convocatoria-oferta', { id: convocatoria.value.id }));
             oferta.value = resp.data.oferta;
             sedes.value = oferta.value.map(({ id, title }) => ({ id, title }));
         }
 
-        if (step.value === 3 && formData.value.sede_id != null) {
-            ofertaSede.value = oferta.value.filter((item) => item.id === formData.value.sede_id);
-            carrerasSeleccionadas.value = [];
+        if (step.value === 3 && sede.value != null) {
         }
     }
 }
@@ -126,14 +146,19 @@ function prevStep() {
 
 watch(carrerasSeleccionadas, (newSelected) => {
     if (newSelected.length > 3) {
+        text.value = t('ingreso._solo_tres_opciones_');
         carrerasSeleccionadas.value = newSelected.slice(0, 3);
         snackbar.value = true;
     }
 });
 
-const snackbar = ref(false);
-
-const text = t('ingreso._solo_tres_opciones_');
+watch(sede, (newSede) => {
+    if (newSede != null) {
+        ofertaSede.value = oferta.value.filter((item) => item.id === sede.value.id);
+    } else {
+        carrerasSeleccionadas.value = [];
+    }
+});
 </script>
 <template>
     <v-stepper alt-labels hide-actions v-model="step">
@@ -143,44 +168,60 @@ const text = t('ingreso._solo_tres_opciones_');
             <v-stepper-item :value="2" :title="$t('sede._sede_')"></v-stepper-item>
             <v-divider></v-divider>
             <v-stepper-item :value="3" :title="$t('ingreso._seleccion_carreras_')"></v-stepper-item>
+            <v-divider></v-divider>
+            <v-stepper-item :value="4" :title="$t('ingreso._resumen_')"></v-stepper-item>
         </v-stepper-header>
         <v-stepper-window>
             <v-stepper-window-item :value="1">
                 <v-card :title="$t('ingreso._seleccion_convocatoria_indicacion_')" flat>
-                    <v-form fast-fail ref="form1">
-                        <v-autocomplete
-                            clearable
-                            :label="$t('convocatoria._convocatoria_')"
-                            :items="convocatorias"
-                            v-model="formData.convocatoria_id"
-                            item-title="nombre"
-                            item-value="id"
-                            prepend-icon="mdi-form-dropdown"
-                        ></v-autocomplete>
-                    </v-form>
+                    <v-card-text class="pt-4">
+                        <v-form fast-fail ref="form1">
+                            <v-autocomplete
+                                clearable
+                                :label="$t('convocatoria._convocatoria_')"
+                                :items="convocatorias"
+                                v-model="convocatoria"
+                                return-object
+                                :rules="[(v) => !!v || $t('_campo_requerido_')]"
+                                item-title="nombre"
+                                item-value="id"
+                                prepend-icon="mdi-form-dropdown"
+                            ></v-autocomplete>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn @click="nextStep" rounded variant="tonal" color="primary" append-icon="mdi-chevron-right">
+                            {{ $t('_siguiente_') }}
+                        </v-btn>
+                    </v-card-actions>
                 </v-card>
-                <v-btn @click="nextStep" rounded variant="tonal" color="primary" append-icon="mdi-chevron-right">{{ $t('_siguiente_') }}</v-btn>
             </v-stepper-window-item>
 
             <v-stepper-window-item :value="2">
                 <v-card :title="$t('ingreso._seleccion_sede_indicacion_')" flat>
-                    <v-form fast-fail ref="form2">
-                        <v-autocomplete
-                            clearable
-                            :label="$t('sede._sede_')"
-                            :items="sedes"
-                            v-model="formData.sede_id"
-                            item-title="title"
-                            item-value="id"
-                            prepend-icon="mdi-form-dropdown"
-                        ></v-autocomplete>
-                    </v-form>
-                    <v-btn class="me-4" rounded variant="tonal" color="secundary" prepend-icon="mdi-chevron-left" @click="prevStep">
-                        {{ $t('_atras_') }}
-                    </v-btn>
-                    <v-btn @click="nextStep" rounded variant="tonal" color="primary" append-icon="mdi-chevron-right">
-                        {{ $t('_siguiente_') }}
-                    </v-btn>
+                    <v-card-text class="pt-4">
+                        <v-form fast-fail ref="form2">
+                            <v-autocomplete
+                                clearable
+                                :label="$t('sede._sede_')"
+                                :items="sedes"
+                                v-model="sede"
+                                return-object
+                                :rules="[(v) => !!v || $t('_campo_requerido_')]"
+                                item-title="title"
+                                item-value="id"
+                                prepend-icon="mdi-form-dropdown"
+                            ></v-autocomplete>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn class="me-4" rounded variant="tonal" color="secundary" prepend-icon="mdi-chevron-left" @click="prevStep">
+                            {{ $t('_atras_') }}
+                        </v-btn>
+                        <v-btn @click="nextStep" rounded variant="tonal" color="primary" append-icon="mdi-chevron-right">
+                            {{ $t('_siguiente_') }}
+                        </v-btn>
+                    </v-card-actions>
                 </v-card>
             </v-stepper-window-item>
 
@@ -218,29 +259,54 @@ const text = t('ingreso._solo_tres_opciones_');
                                 </v-form>
                             </v-col>
                             <v-col class="pa-6" cols="12" md="6">
-                                <div v-if="carrerasSeleccionadas.length > 0">
-                                    <v-icon color="success" size="x-large" icon="mdi-numeric-1-box"></v-icon> {{ $t('ingreso._primera_opcion_') }}:
-                                    <span class="text-success text-high-emphasis font-weight-black">{{ carrerasSeleccionadas[0].title }}</span>
-                                </div>
-                                <div v-if="carrerasSeleccionadas.length > 1">
-                                    <v-icon color="warning" size="x-large" icon="mdi-numeric-2-box"></v-icon> {{ $t('ingreso._segunda_opcion_') }}:
-                                    <span class="text-warning text-high-emphasis font-weight-black">{{ carrerasSeleccionadas[1].title }}</span>
-                                </div>
-                                <div v-if="carrerasSeleccionadas.length > 2">
-                                    <v-icon color="primary" size="x-large" icon="mdi-numeric-3-box"></v-icon> {{ $t('ingreso._tercera_opcion_') }}:
-                                    <span class="text-high-emphasis font-weight-black text-primary"> {{ carrerasSeleccionadas[2].title }}</span>
-                                </div>
+                                <CarrerasSeleccionadas :carrerasSeleccionadas="carrerasSeleccionadas"></CarrerasSeleccionadas>
                             </v-col>
                         </v-row>
                     </v-card-text>
-                    <v-btn class="me-4" rounded variant="tonal" color="secundary" prepend-icon="mdi-chevron-left" @click="prevStep">
-                        {{ $t('_atras_') }}
-                    </v-btn>
-                    <v-col cols="12" align="right">
-                        <v-btn :loading="loading" rounded variant="tonal" color="blue-darken-4" prepend-icon="mdi-content-save">
+                    <v-card-actions>
+                        <v-btn class="me-4" rounded variant="tonal" color="secundary" prepend-icon="mdi-chevron-left" @click="prevStep">
+                            {{ $t('_atras_') }}
+                        </v-btn>
+                        <v-btn @click="validarCarreras" rounded variant="tonal" color="primary" append-icon="mdi-chevron-right">
+                            {{ $t('_siguiente_') }}
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-stepper-window-item>
+            <v-stepper-window-item :value="4">
+                <v-card :title="$t('ingreso._resumen_')" flat>
+                    <v-card-text class="pt-4">
+                        {{ $t('ingreso._revise_informacion_') }}
+                        <v-alert border="start" type="info" variant="outlined">
+                            <template v-slot:title> {{ $t('ingreso._seleccion_convocatoria_') }} </template>
+
+                            <span class="text-h6 text-black">{{ convocatoria.nombre }} -- {{ convocatoria.descripcion }}</span>
+                        </v-alert>
+
+                        <br />
+
+                        <v-alert border="start" type="info" variant="outlined">
+                            <template v-slot:title> {{ $t('sede._sede_') }} </template>
+                            <span class="text-h6 text-black">{{ sede.title }}</span>
+                        </v-alert>
+
+                        <br />
+
+                        <v-alert border="start" type="info" variant="outlined">
+                            <template v-slot:title> {{ $t('ingreso._seleccion_carreras_') }} </template>
+                            <CarrerasSeleccionadas :carrerasSeleccionadas="carrerasSeleccionadas"></CarrerasSeleccionadas>
+                        </v-alert>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn class="me-4" rounded variant="tonal" color="secundary" prepend-icon="mdi-chevron-left" @click="prevStep">
+                            {{ $t('_atras_') }}
+                        </v-btn>
+
+                        <v-spacer></v-spacer>
+                        <v-btn :loading="loading" rounded variant="tonal" color="blue-darken-4" prepend-icon="mdi-content-save" @click="submitForm">
                             {{ $t('_guardar_') }}
                         </v-btn>
-                    </v-col>
+                    </v-card-actions>
                 </v-card>
             </v-stepper-window-item>
         </v-stepper-window>
