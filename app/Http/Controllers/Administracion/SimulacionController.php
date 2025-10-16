2acdc2dcb7da8f7492e5d590ae9d4ca1b8e8c4f9
Erick Pérez <erick.gperez@outlook.com>
2025-10-16 09:23:38 -0600

    Terminada la simulación de registro de solicitudes de ingreso

--- app/Http/Controllers/Administracion/SimulacionController.php
+++ app/Http/Controllers/Administracion/SimulacionController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Administracion;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Ingreso\AspiranteController;
+use App\Models\Academica\CarreraSede;
+use App\Models\Academica\Sede;
+use App\Models\Ingreso\Convocatoria;
 use App\Models\Rol;
 use App\Models\Secundaria\DataBachillerato;
 use App\Models\User;
@@ -31,10 +34,16 @@ class SimulacionController extends Controller
         if (Auth::user()->hasRole('super-admin')) {
 
             $rolAspirante = Rol::where('name', 'aspirante')->first();
+            //Elegir una sede al azar
+            $sede = Sede::inRandomOrder()->first();
+
+            //Elegir al azar 3 carreras de esa sede
+            $carreras = CarreraSede::inRandomOrder()->where('sede_id', $sede->id)->take(3)->get();
+
             echo 'Simulacion de datos pre-seleccion';
 
             //Verificar que se haya cargado la base de bachilleres
-            $bachilleres = DataBachillerato::whereNotNull('correo')->limit(10)->get();
+            $bachilleres = DataBachillerato::whereNotNull('correo')->limit(1000)->get();
 
             if ($bachilleres->isEmpty()) {
                 echo '<BR><BR>No se ha cargado la base de bachilleres, primero suba el archivo en Ingreso->convocatoria->cargar archivo';
@@ -63,28 +72,35 @@ class SimulacionController extends Controller
                     $persona = $user->personas()->first();
 
                     //Crear la solicitud de ingreso
+                    $aspiranteC = new AspiranteController();
                     //Verificar si ya tiene una
                     $solicitud = $persona->solicitudes()->with('estado', 'etapa', 'persona')
                         ->where('rol_id', $rolAspirante->id)
                         ->orderBy('created_at', 'desc')
                         ->first();
                     if (!$solicitud) {
-                        $aspiranteC = new AspiranteController();
                         $aspiranteC->solicitudCrear($persona->id);
                     }
                     //Volver a recuperar para actualizar datos
-                    $solicitud = $persona->solicitudes()->with('estado', 'etapa', 'persona')
-                        ->where('rol_id', $rolAspirante->id)
-                        ->orderBy('created_at', 'desc')
-                        ->first();
+                    $solicitud->fresh();
+
+                    //Recuperar la convocatoria de prueba
+                    $convocatoria = Convocatoria::where('nombre', '01-2026')->first();
+                    $carrera_sede = [];
+                    foreach ($carreras as $c) {
+                        $carrera_sede[] = $c->id;
+                    }
 
                     //Crear la seleccion
                     $data = [
-                        'convocatoria_id' => 'value1',
-                        'carrera_sede' => 'value2',
+                        'convocatoria_id' => $convocatoria->id,
+                        'carrera_sede' => $carrera_sede,
                     ];
 
-                    //$request = Request::create(route('ingreso-solicitud-seleccion-carrera', ['id' => $solicitud->id]), 'POST', $data);
+                    $request = Request::create(route('ingreso-solicitud-seleccion-carrera', ['id' => $solicitud->id]), 'POST', $data);
+                    $aspiranteC->seleccionCarrera($solicitud->id, $request);
+
+                    echo '<BR><BR> Se han cargado todos los datos de la simulación. Ahora ya se puede realizar la selección de aspirantes';
                 }
             }
         } else {
