a282139de94d680789f74ef8a28a3e20ce356079
Erick Pérez <erick.gperez@outlook.com>
2025-10-23 08:46:56 -0600

    Agregado nuevo campo a ingreso.aspirante

--- database/migrations/ingreso/2025_09_22_192151_ingreso.aspirante.php
+++ database/migrations/ingreso/2025_09_22_192151_ingreso.aspirante.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->comment('Tabla de aspirantes a una convocatoria de ingreso');
             $table->string('nie', length: 30)->unique()->nullable()->comment('Número de identificación de estudiante');
             $table->float('calificacion_bachillerato', 5, 2)->nullable()->comment('Calificación obtenida en la prueba que se realiza en educación media');
+            $table->boolean('seleccionado')->default(false)->comment('Indica si el aspirante ha sido seleccionado como estudiante');
             $table->foreignId('persona_id')->comment('Persona a la que está asignado el aspirante');
             $table->foreign('persona_id')->references('id')->on('public.persona')->onDelete('RESTRICT')->onUpdate('CASCADE');
 
