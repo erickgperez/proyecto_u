4c8d55fe2c8371b695b63e56ac33f3cc5c052b40
Erick Pérez <erick.gperez@outlook.com>
2025-10-27 13:31:19 -0600

    Agregar campos de auditoría a tablas aspirante y convocatoria_aspirante

--- database/migrations/ingreso/2025_09_22_192151_ingreso.aspirante.php
+++ database/migrations/ingreso/2025_09_22_192151_ingreso.aspirante.php
@@ -20,6 +20,11 @@ return new class extends Migration
             $table->foreignId('persona_id')->comment('Persona a la que está asignado el aspirante');
             $table->foreign('persona_id')->references('id')->on('public.persona')->onDelete('RESTRICT')->onUpdate('CASCADE');
 
+            $table->unsignedBigInteger('created_by')->nullable()->comment('Usuario que creó el registro');
+            $table->unsignedBigInteger('updated_by')->nullable()->comment('Usuario que realizó la última actualización del registro');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
+
             $table->timestamps();
         });
     }
--- database/migrations/ingreso/2025_09_27_191958_ingreso.convocatoria_aspirante.php
+++ database/migrations/ingreso/2025_09_27_191958_ingreso.convocatoria_aspirante.php
@@ -27,6 +27,11 @@ return new class extends Migration
             $table->foreignId('carrera_sede_id')->comment('Carrera sede en que fue seleccionada')->nullable();
             $table->foreign('carrera_sede_id')->references('id')->on('workflow.solicitud_carrera_sede')->onDelete('CASCADE')->onUpdate('CASCADE');
 
+            $table->unsignedBigInteger('created_by')->nullable()->comment('Usuario que creó el registro');
+            $table->unsignedBigInteger('updated_by')->nullable()->comment('Usuario que realizó la última actualización del registro');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
+
             $table->timestamps();
         });
     }
