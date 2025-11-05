d9013920f29170398697d7a48310d37f69dc8524
Erick Pérez <erick.gperez@outlook.com>
2025-11-04 20:16:07 -0600

    Creación de tabla ingreso.relacion_carreras

---
+++ database/migrations/ingreso/2025_04_11_091958_ingreso.relacion_carreras.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ingreso.relacion_carreras', function (Blueprint $table) {
+            $table->id();
+
+            $table->comment('Relación para definir relaciones entre carreras universitarias y carreras de bachillerato');
+
+            $table->foreignId('carrera_universitaria_id')->comment('Id de la carrera universitaria');
+            $table->foreign('carrera_universitaria_id')->references('id')->on('plan_estudio.carrera')->onDelete('CASCADE')->onUpdate('CASCADE');
+
+            $table->foreignId('carrera_secundaria_id')->comment('Id de la carrera de bachillerato');
+            $table->foreign('carrera_secundaria_id')->references('id')->on('secundaria.carrera')->onDelete('CASCADE')->onUpdate('CASCADE');
+
+            $table->unsignedBigInteger('created_by')->nullable()->comment('Usuario que creó el registro');
+            $table->unsignedBigInteger('updated_by')->nullable()->comment('Usuario que realizó la última actualización del registro');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingreso.relacion_carreras');
+    }
+};
