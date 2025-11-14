bc63adec37b7630b9869e692087a68d5f6556dae
Erick Pérez <erick.gperez@outlook.com>
2025-11-14 08:31:04 -0600

    Creación de tabla semestre

---
+++ app/Models/Academica/Semestre.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\Ingreso;
+
+use App\Models\Calendarizacion;
+use App\Models\User;
+use App\Traits\UserStamps;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Semestre extends Model
+{
+    use UserStamps;
+
+    protected $table = "academico.semestre";
+
+    protected $fillable = [
+        'codigo',
+        'descripcion',
+        'fecha_inicio',
+        'fecha_fin',
+        'calendarizacion_id'
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function calendario(): BelongsTo
+    {
+        return $this->belongsTo(Calendarizacion::class, 'calendarizacion_id');
+    }
+}
---
+++ database/migrations/academico/2025_11_14_203153_academico.semestre.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('academico.semestre', function (Blueprint $table) {
+            $table->id();
+            $table->comment('Los semestres o periodos académicos');
+            $table->string('codigo', length: 100)->unique()->comment('Código identificador del semestre');
+            $table->string('descripcion', length: 255)->nullable()->comment('Texto descriptivo del semestre');
+            $table->timestamp('fecha_inicio')->comment('Fecha de inicio del semestre');
+            $table->timestamp('fecha_fin')->comment('Fecha de finalizacion del semestre');
+
+            $table->foreignId('calendarizacion_id')->nullable()->comment('Calendario de eventos del semestre');
+            $table->foreign('calendarizacion_id')->references('id')->on('public.calendarizacion')->onDelete('RESTRICT')->onUpdate('CASCADE');
+
+            $table->unsignedBigInteger('created_by')->nullable()->comment('Usuario que creó el registro');
+            $table->unsignedBigInteger('updated_by')->nullable()->comment('Usuario que realizó la última actualización del registro');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('academico.semestre');
+    }
+};
