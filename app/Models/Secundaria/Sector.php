23db06de40ea87daec798e84e1bd71ff5da798f8
Erick Pérez <erick.gperez@outlook.com>
2025-11-21 11:37:02 -0600

    Corrección de error

--- app/Models/Secundaria/Sector.php
+++ app/Models/Secundaria/Sector.php
@@ -2,10 +2,13 @@
 
 namespace App\Models\Secundaria;
 
+use App\Traits\HasCreateMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Sector extends Model
 {
+
+    use HasCreateMany;
 
     protected $table = "secundaria.sector";
 }
