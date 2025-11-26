62947ddad66b7ff3e5a4d1272274ac2a719000ff
Erick Pérez <erick.gperez@outlook.com>
2025-11-25 21:12:02 -0600

    Carga de datos iniciales para tipos de documentos

--- database/seeders/Documento/TipoSeeder.php
+++ database/seeders/Documento/TipoSeeder.php
@@ -15,16 +15,17 @@ class TipoSeeder extends Seeder
     public function run(): void
     {
         TipoDocumento::createMany([
-            ['codigo' => 'CARNE', 'descripcion' => 'Carné de identificación'],
-            ['codigo' => 'EXAMEN_MEDICO', 'descripcion' => 'Exámenes médicos'],
-            ['codigo' => 'DIPLOMA_BACHILLERATO', 'descripcion' => 'Diploma de bachillerato'],
-            ['codigo' => 'FOTOGRAFIA', 'descripcion' => 'Fotografía'],
-            ['codigo' => 'DUI', 'descripcion' => 'Documento único de identidad'],
-            ['codigo' => 'NIT', 'descripcion' => 'NIT homologado'],
-            ['codigo' => 'AFP', 'descripcion' => 'Carné de AFP'],
-            ['codigo' => 'TITULO_BACHILLER', 'descripcion' => 'Título de bachillerato'],
-            ['codigo' => 'PARTIDA_NACIMIENTO', 'descripcion' => 'Partida de nacimiento'],
-            ['codigo' => 'TITULO_UNIVERSITARIO', 'descripcion' => 'Título universitario'],
+            ['codigo' => 'CARNE', 'descripcion' => 'Carné de identificación', 'multiple' => false],
+            ['codigo' => 'EXAMEN_MEDICO', 'descripcion' => 'Exámenes médicos', 'multiple' => false],
+            ['codigo' => 'DIPLOMA_BACHILLERATO', 'descripcion' => 'Diploma de bachillerato', 'multiple' => false],
+            ['codigo' => 'FOTOGRAFIA', 'descripcion' => 'Fotografía', 'multiple' => false],
+            ['codigo' => 'DUI', 'descripcion' => 'Documento único de identidad', 'multiple' => false],
+            ['codigo' => 'NIT', 'descripcion' => 'NIT homologado', 'multiple' => false],
+            ['codigo' => 'AFP', 'descripcion' => 'Carné de AFP', 'multiple' => false],
+            ['codigo' => 'TITULO_BACHILLER', 'descripcion' => 'Título de bachillerato', 'multiple' => false],
+            ['codigo' => 'PARTIDA_NACIMIENTO', 'descripcion' => 'Partida de nacimiento', 'multiple' => false],
+            ['codigo' => 'TITULO_UNIVERSITARIO', 'descripcion' => 'Título universitario', 'multiple' => true],
+            ['codigo' => 'OTROS_ATESTADOS', 'descripcion' => 'Otros atestados', 'multiple' => true],
         ]);
 
         $rolDocente = Rol::where('name', 'docente')->where('guard_name', 'web')->first();
@@ -32,7 +33,8 @@ class TipoSeeder extends Seeder
             ['rol_id' => $rolDocente->id, 'tipo_id' => TipoDocumento::where('codigo', 'DUI')->first()->id],
             ['rol_id' => $rolDocente->id, 'tipo_id' => TipoDocumento::where('codigo', 'NIT')->first()->id],
             ['rol_id' => $rolDocente->id, 'tipo_id' => TipoDocumento::where('codigo', 'AFP')->first()->id],
-            ['rol_id' => $rolDocente->id, 'tipo_id' => TipoDocumento::where('codigo', 'TITULO_UNIVERSITARIO')->first()->id]
+            ['rol_id' => $rolDocente->id, 'tipo_id' => TipoDocumento::where('codigo', 'TITULO_UNIVERSITARIO')->first()->id],
+            ['rol_id' => $rolDocente->id, 'tipo_id' => TipoDocumento::where('codigo', 'OTROS_ATESTADOS')->first()->id]
         ]);
 
         $rolEstudiante = Rol::where('name', 'estudiante')->where('guard_name', 'web')->first();
